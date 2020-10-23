cc153cea7db9bfb0dd874777ef55374cdcf22f82
jbogdanic <josipb683@gmail.com>
2020-10-23 19:23:05 +0200

    Added some stuff

---
+++ pizza-time/src/components/AddPizza.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+         <b-field label="Name">
+            <b-input v-model="name" placeholder="Upisite ime Pizze"></b-input>
+        </b-field>
+        <b-button>Nova Pizza</b-button>
+    </form>
+</template>
+
+<script>
+ export default{
+     data() {
+         return {
+             
+         }
+     }
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
